e0600a2f218c3d4b82433187437e8446ddf62b7d
douglasdsbraga <oladouglasbraga@gmail.com>
2025-01-09 11:19:41 -0300

    Ajustes finais - agora dá pra reformualr a home

--- assets/js/prod/caroucel.js
+++ assets/js/prod/caroucel.js
@@ -29,3 +29,25 @@ $('#clientes').owlCarousel({
 // $(".ctaEventos-prev").click(function(){
 //     caroucelEventosSlide.trigger('prev.owl.carousel');
 // })
+
+$('#iniciativas').owlCarousel({
+    loop:true,
+    margin: 30,
+    // autoWidth:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        740:{
+            items:1
+        },
+        1170:{
+            items:2,
+            loop:false
+        }
+    }
+});
