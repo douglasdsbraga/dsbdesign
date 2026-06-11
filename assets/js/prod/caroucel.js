d61c507726630f89f80e7ff1951a1419f1d2c0b2
douglasdsbraga <oladouglasbraga@gmail.com>
2026-06-11 09:10:41 -0300

    Update

--- assets/js/prod/caroucel.js
+++ assets/js/prod/caroucel.js
@@ -30,6 +30,28 @@ $('#clientes').owlCarousel({
 //     caroucelEventosSlide.trigger('prev.owl.carousel');
 // })
 
+$('#artigos-linkedin').owlCarousel({
+    loop:true,
+    margin: 30,
+    // autoWidth:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        740:{
+            items:2
+        },
+        1170:{
+            items:2,
+            loop:false
+        }
+    }
+});
+
 $('#iniciativas').owlCarousel({
     loop:true,
     margin: 30,
